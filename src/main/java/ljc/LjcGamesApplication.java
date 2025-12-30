8bb3d7cac1a6251e10793821e5f17846ac99b78b
ljc <1290140456@qq.com>
2025-12-31 07:02:33 +0800

    feat: project init with clean structure

---
+++ src/main/java/ljc/LjcGamesApplication.java
@@ -0,0 +1,13 @@
+package ljc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjcGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LjcGamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ljc/LjcGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package ljc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LjcGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
