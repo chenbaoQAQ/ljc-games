195b61a9c9046e1037a39422c0ff5970903e5bda
chenbaoQAQ <junchenli180@gmail.com>
2026-01-31 12:15:39 +0800

    根据v2.2调整合理架构

--- ljc-games-server/src/main/java/ljc/repository/GeneralTemplateRepository.java
+++
@@ -1,8 +0,0 @@
-package ljc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GeneralTemplateRepository extends JpaRepository<GeneralTemplate, Integer> {
-}
--- ljc-games-server/src/main/java/ljc/repository/UserGeneralRepository.java
+++
@@ -1,9 +0,0 @@
-package ljc.repository;
-
-import ljc.entity.UserGeneral;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserGeneralRepository extends JpaRepository<UserGeneral, Integer> {
-}
